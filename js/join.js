54550a88e240fd97bc9a6b351bd5f8acd2f9811c
Bogyoi <shson2280@gmail.com>
2024-01-15 20:23:28 +0900

    불필요한 코드 삭제, textContent -> innerText로 변경, 변수 이름 변경

--- js/join.js
+++ js/join.js
@@ -1,176 +1,124 @@
-function validateSignupId() {
+function validSignupId() {
   const signupIdInput = document.getElementById('id');
   const idError = document.getElementById('idNofi');
   const idPattern = /^[a-zA-Z0-9]{6,}$/;
 
   if (!idPattern.test(signupIdInput.value)) {
-    idError.textContent = '* 영문과 숫자만 사용 가능, 최소 6글자 이상 입력하세요';
+    idError.innerText = '* 영문과 숫자만 사용 가능, 최소 6글자 이상 입력하세요';
   } else {
-    idError.textContent = '';
+    idError.innerText = '';
   }
 }
 
-function validateSignupPassword() {
+function validSignupPassword() {
   const signupPasswordInput = document.getElementById('pw');
   const passwordError = document.getElementById('pwNofi');
-  // 비밀번호는 최소 8글자 이상이며, 영문 대문자, 소문자, 숫자, 특수문자 중 하나 이상을 포함해야 합니다.
   const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
   if (!passwordPattern.test(signupPasswordInput.value)) {
-    passwordError.textContent = '* 비밀번호는 최소 8글자 이상이며, 영문 대문자, 소문자, 숫자, 특수문자를 하나 이상 포함해야 합니다.';
+    passwordError.innerText = '* 비밀번호는 최소 8글자 이상이며, 영문 대문자, 소문자, 숫자, 특수문자를 하나 이상 포함해야 합니다.';
   } else {
-    passwordError.textContent = '';
+    passwordError.innerText = '';
   }
 }
 
-function validateConfirmPassword() {
+function validConfirmPassword() {
   const signupPasswordInput = document.getElementById('pw');
   const confirmPasswordInput = document.getElementById('confirmPw');
   const confirmPasswordError = document.getElementById('confirmPwNofi');
 
   if (signupPasswordInput.value !== confirmPasswordInput.value) {
-    confirmPasswordError.textContent = '* 입력한 비밀번호와 일치하지 않습니다.';
+    confirmPasswordError.innerText = '* 입력한 비밀번호와 일치하지 않습니다.';
   } else {
-    confirmPasswordError.textContent = '';
+    confirmPasswordError.innerText = '';
   }
 }
 
-function formatPhoneNumber() {
+function validformatTel() {
   const telInput = document.getElementById('tel');
   const telError = document.getElementById('telNofi')
-  const telPattern = /^\d+$/; // 숫자로만 이루어진 패턴
-  /*   telInput.value = telInput.value.replace(/[^0-9]/g, ''); // 숫자 이외의 문자 제거 
-    telInput.value = telInput.value.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'); // 자동으로 '-' 추가 */
-  /*   const match = cleaned.match(/^(\d{3})(\d{4})(\d{4})$/);
-    if (match) {
-      telInput.value = `${match[1]}-${match[2]}-${match[3]}`;
-    } */
+  const telPattern = /^\d+$/;
   if (!telPattern.test(telInput.value)) {
-    telError.textContent = '* 숫자만 입력하세요 ("-" 생략)';
+    telError.innerText = '* 숫자만 입력하세요 ("-" 생략)';
   } else {
-    telError.textContent = '';
+    telError.innerText = '';
   }
 }
 
-function validateEmail() {
+function validEmail() {
   const emailInput = document.getElementById('email');
   const emailError = document.getElementById('emailNofi');
   const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-  //  /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 
   if (!emailPattern.test(emailInput.value)) {
-    emailError.textContent = '* 올바른 이메일 형식이 아닙니다.';
+    emailError.innerText = '* 올바른 이메일 형식이 아닙니다.';
   } else {
-    emailError.textContent = '';
+    emailError.innerText = '';
   }
 }
 
 async function signup() {
-  // 유효성 검사
-  validateSignupId();
-  validateSignupPassword();
-  validateConfirmPassword();
+  validSignupId();
+  validSignupPassword();
+  validConfirmPassword();
 
-  const idError = document.getElementById('idNofi').textContent;
-  const passwordError = document.getElementById('pwNofi').textContent;
-  const confirmPasswordError = document.getElementById('confirmPwNofi').textContent;
+  const idError = document.getElementById('idNofi').innerText;
+  const passwordError = document.getElementById('pwNofi').innerText;
+  const confirmPasswordError = document.getElementById('confirmPwNofi').innerText;
 
   if (idError || passwordError || confirmPasswordError) {
     alert('입력값을 확인하세요.');
     return;
   }
 
-  /*   // 회원가입 성공 시 로컬 스토리지에 저장
-    const signupId = document.getElementById('signupId').value;
-    const signupPassword = document.getElementById('signupPassword').value;
-    const name = document.getElementById('name').value;
-    const tel = document.getElementById('tel').value;
-    const email = document.getElementById('email').value;
-    const address = document.getElementById('address').value;
-
-    const userData = {
-      id: signupId,
-      password: signupPassword,
-      name: name,
-      tel: tel,
-      email: email,
-      address: address
-    };
-
-    localStorage.setItem(signupId, JSON.stringify(userData)); */
-
-  // 회원가입 성공 시 기존 JSON 파일 불러오기
-  let jsonData = await fetchJsonFile();
-
   /////////////////////////////////////////////////////////////////
+  let Datas = await storageData();
   // 입력된 아이디가 이미 존재하는지 확인
-
-
   // 회원가입 성공 시 JSON 파일 리스트에 추가
-  const signupId = document.getElementById('id').value;
-  const isDuplicateId = jsonData.some(user => user.id === signupId);
-
+  const newid = document.getElementById('id').value;
+  const isDuplicateId = Datas.some(user => user.id === newid);
   if (isDuplicateId) {
     alert('이미 존재하는 아이디입니다. 다른 아이디를 사용해주세요.');
     return;
   }
-  const signupPassword = document.getElementById('pw').value;
-  const name = document.getElementById('name').value;
-  // const tel = document.getElementById('tel').value;
-  /*   tel = tel.replace(/[^0-9]/g, ''); // 숫자 이외의 문자 제거
-    tel = tel.replace(/(\d{3})(\d{4})(\d{4})/, '$1-$2-$3'); // 자동으로 '-' 추가 */
-  const tel = document.getElementById('tel').value.replace(/[^0-9]/g, ''); // 숫자 이외의 문자 제거
-  signupTel = tel.replace(/(\d{2,3})(\d{3,4})(\d{4})/, `$1-$2-$3`); // 자동으로 '-' 추가
-  const email = document.getElementById('email').value;
-  const address = document.getElementById('address').value;
+  const newpw = document.getElementById('pw').value;
+  const newname = document.getElementById('name').value;
+  const tel = document.getElementById('tel').value.replace(/[^0-9]/g, ''); // value.replace로 문자열을 다른 문자열로 교체, 숫자 이외의 문자 제거
+  newtel = tel.replace(/(\d{2,3})(\d{3,4})(\d{4})/, `$1-$2-$3`); //  '-' 추가
+  const newemail = document.getElementById('email').value;
+  const newaddress = document.getElementById('address').value;
 
   // 기존 데이터 가져오기
-  //let jsonData = JSON.parse(localStorage.getItem('userData')) || [];
-  jsonData = jsonData || []; ////jsonData가 값이 존재하면 그 값을 그대로 사용하고, 값이 존재하지 않으면 빈 배열 
-  jsonData = jsonData.filter(user => user.id !== signupId); //중복 제거
+  Datas = Datas.filter(user => user.id !== newid); //중복 제거
   // 새로운 데이터 추가
   const userData = {
-    id: signupId,
-    password: signupPassword,
-    name: name,
-    tel: signupTel,
-    email: email,
-    address: address
+    id: newid,
+    password: newpw,
+    name: newname,
+    tel: newtel,
+    email: newemail,
+    address: newaddress
   };
-  jsonData.push(userData);
-  localStorage.setItem('userList', JSON.stringify(jsonData));
+  Datas.push(userData);
+  localStorage.setItem('userList', JSON.stringify(Datas));
   alert('회원가입에 성공했습니다!');
 }
 
-// 비동기적으로 JSON 파일을 가져오는 함수
-/* async function fetchJsonFile() {
+async function storageData() {
   try {
-    const response = await fetch('/homework/member.json');
-    const jsonData = await response.json();
-    return jsonData;
-  } catch (error) {
-    console.error('Error fetching JSON file:', error);
-    return null;
-  }
-} */
-async function fetchJsonFile() {
-  try {
-    const response = await fetch('/member.json');
-    const serverData = await response.json(); ///////////////////////
-
+    const serverData = await (await fetch('/member.json')).json();
     // 로컬 스토리지에서 이전 데이터 가져오기
-    const localData = JSON.parse(localStorage.getItem('userList')) || [];
+    const localData = JSON.parse(localStorage.getItem('userList'));
 
-    // 서버에서 가져온 데이터와 로컬 스토리지에 있는 데이터 병합
-    // const mergedData = [...localData, ...serverData];
-     // 서버에서 가져온 데이터와 로컬 스토리지에 있는 데이터 중복 제거 후 병합
-    const mergedData = [
-      ...localData.filter(user => !serverData.some(s => s.id === user.id)), 
-      ...serverData];
+    // 서버에서 가져온 데이터와 로컬 스토리지에 있는 데이터 중복 제거 후 병합
+    const mergedData = [ //filter를 사용해 아이디가 중복되지않는 데이터만 가져옴( localData 배열에서 severData에 있는 아이디와 중복되지 않는 사용자만 필터링)
+      ...serverData,
+      ...localData.filter(user => !serverData.some(s => s.id === user.id)), //중복되는게 하나라도 없는걸 선택-> 즉, 새롭게 가입한 사용자가 들어가게됨
+    ];
 
     return mergedData;
   } catch (error) {
-    console.error('Error fetching JSON file:', error);
-    return null;
+    console.error(error);
+    return;
   }
 }
