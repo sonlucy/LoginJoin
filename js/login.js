0ed2093b73d3c838a98a8251581498ac64b8bdeb
Bogyoi <shson2280@gmail.com>
2024-01-15 20:26:08 +0900

    불필요한 코드 삭제, 로그인 성공 시 회원이름과 함께 alert 띄우기, textContent -> innerText로 변경

--- js/login.js
+++ js/login.js
@@ -1,21 +1,9 @@
-/* // 페이지 로딩 시 실행
-document.addEventListener('DOMContentLoaded', async function () {
-  const jsonData = await fetchJsonFile();
-
-  if (!jsonData) {
-    console.error('Failed to load JSON data.');
-    return;
-  }
-
-  // 로컬 스토리지에 JSON 데이터 저장
-  localStorage.setItem('userList', JSON.stringify(jsonData));
-}); */
 // 페이지 로딩 시 실행
 document.addEventListener('DOMContentLoaded', async function () {
   // 기존 데이터 가져오기 또는 초기화. 이 부분 살리면 로그인 페이지로 가면 회원가입 한 정보가 로컬에서 사라짐
-  const jsonData = await fetchJsonFile();
-  localStorage.setItem('userList', JSON.stringify(jsonData));
-
+/*   const jsonData = await fetchJsonFile();
+  localStorage.setItem('userList', JSON.stringify(jsonData)); //로컬스토리지에 JSON 데이터 저장
+   */
   let userList = [];
   const storedData = localStorage.getItem('userList');
   if (storedData) {
@@ -26,101 +14,54 @@ document.addEventListener('DOMContentLoaded', async function () {
   localStorage.setItem('userList', JSON.stringify(userList));
 });
 
-function validateId() {
+///////////////////////join.js의 함수와 형태가 같음////////////////////////////
+function validId() {
   const idInput = document.getElementById('id');
   const idError = document.getElementById('idError');
   const idPattern = /^[a-zA-Z0-9]{6,}$/;
 
   if (!idPattern.test(idInput.value)) {
-    idError.textContent = '* ID는 영문/숫자만 사용, 최소 6글자 이상만 입력하세요';
+    idError.innerText = '* ID는 영문/숫자만 사용, 최소 6글자 이상만 입력하세요';
   } else {
-    idError.textContent = '';
+    idError.innerText = '';
   }
 }
 
-function validatePassword() {
+function validPassword() {
   const pwInput = document.getElementById('pw');
   const pwError = document.getElementById('pwError');
-  // 비밀번호는 최소 8글자 이상이며, 영문 대문자, 소문자, 숫자, 특수문자 중 하나 이상을 포함해야 합니다.
   const pwPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
 
   if (!pwPattern.test(pwInput.value)) {
-    pwError.textContent = '* 비밀번호는 최소 8글자 이상이며, 영문 대문자, 소문자, 숫자, 특수문자를 하나 이상 포함해야 합니다.';
+    pwError.innerText = '* 비밀번호는 최소 8글자 이상이며, 영문 대문자, 소문자, 숫자, 특수문자를 하나 이상 포함해야 합니다.';
   } else {
-    pwError.textContent = '';
+    pwError.innerText = '';
   }
 }
+///////////////////////////////////////////////////////////////////////
 
-
-// JSON 데이터를 가져와서 저장
 async function fetchJsonFile() {
-  try {
-    const response = await fetch('./member.json');
-    const jsonData = await response.json();
-    return jsonData;
-  } catch (error) {
-    console.error('Error fetching JSON file:', error);
-    return null;
-  }
+  return fetch('/member.json')
+    .then(response => response.json())
+    .catch(error => {
+      console.error(error);
+    });
 }
-/* 
-// 로그인 함수
-async function login() {
-  const jsonData = await fetchJsonFile();
 
-  if (!jsonData) {
-    alert('로그인에 실패했습니다. 다시 시도해주세요.');
-    return;
-  }
-
-  const enteredId = document.getElementById('id').value;
-  const enteredPw = document.getElementById('pw').value;
-
-  // JSON 데이터에서 ID와 비밀번호 확인
-  const user = jsonData.find(u => u.id === enteredId && u.password === enteredPw);
-
-  if (user) {
-    alert('로그인에 성공했습니다!');
-    // 로컬 스토리지에 사용자 정보 저장
-    // localStorage.setItem('currentUser', JSON.stringify(user));
-    // 기존 데이터 가져오기 또는 초기화
-    let userList = [];
-    const storedData = localStorage.getItem('userList');
-    if (storedData) {
-      userList = JSON.parse(storedData);
-    }
-
-    // 새로운 데이터 추가
-    userList.push(user);
-
-    // 로컬 스토리지에 저장
-    localStorage.setItem('userList', JSON.stringify(userList));
-  } else {
-    alert('아이디 또는 비밀번호가 잘못되었습니다. 다시 시도해주세요.');
-  }
- */
 // 로그인 함수
 async function login() {
 
   const enteredId = document.getElementById('id').value;
   const enteredPw = document.getElementById('pw').value;
-
-  //로컬 스토리지에 json 파일 저장
-/*   const jsonData = await fetchJsonFile();
-  localStorage.setItem('userList', JSON.stringify(jsonData)); */
 
   // 로컬 스토리지에서 회원 정보 가져오기
   const storedData = localStorage.getItem('userList');
-  // const userList = storedData ? JSON.parse(storedData) : [];
   let userList = JSON.parse(storedData);
-  localStorage.setItem('userList', JSON.stringify(userList)); //새롭게 로컬스토리지 세팅
-
   // JSON 데이터에서 ID와 비밀번호 확인
-  const user = userList.find(u => u.id === enteredId && u.password === enteredPw);
-
-
+  const user = userList.find(u =>
+    u.id === enteredId && u.password === enteredPw);
   if (user) {
-    alert('로그인에 성공했습니다!');
+    alert(`${user.name}님 반갑습니다.`); 
   } else {
     alert('아이디 또는 비밀번호가 잘못되었습니다. 다시 시도해주세요.');
   }
